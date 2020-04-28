cb8c80243ae85dfbc6144c64407a8e91a9efd96a
David Souza <souzanaranjo@gmail.com>
2020-04-27 21:19:19 -0500

    Updates

---
+++ precache-manifest.ab2c24d37be02de7b2bc5e181bdd8027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac696b587805e6140644a39ce19a1cd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84ec0071aff1de543119",
+    "url": "/monsters-rolodex/static/css/main.e1a698e2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ec0071aff1de543119",
+    "url": "/monsters-rolodex/static/js/main.073ba252.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
